11f3ae50dbab456ab27419ea73db150501c08d29
𝐯𝐚𝐥𝐢𝐝𝐨𝐭𝐠𝐨 <validotgo@mail.com>
2024-12-15 01:38:53 +0300

    Create Pingpong.sh

---
+++ Pingpong.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Program URL'si
+PROGRAM_URL="https://pingpong-build.s3.ap-southeast-1.amazonaws.com/linux/latest/PINGPONG"
+
+# Programı indirme
+wget $PROGRAM_URL -O PINGPONG
+
+# Screen başlatma
+screen -dmS pingpong
+
+# Screen içerisine komutları gönderme
+screen -S pingpong -X stuff "chmod +x ./PINGPONG && ./PINGPONG --key"
+screen -S pingpong -X stuff "\n"
+
+# Kullanıcıya bilgi verme
+echo "Script çalıştırıldı. Screen içinde 'chmod +x ./PINGPONG && ./PINGPONG --key' komutu hazırlandı ve bekliyor."
+
+# Screen'e bağlanma
+screen -r pingpong
