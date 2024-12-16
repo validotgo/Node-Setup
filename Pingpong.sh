c0817b8438bddd651c1f82ab4c53f889f67cc1e8
𝐯𝐚𝐥𝐢𝐝𝐨𝐭𝐠𝐨 <validotgo@mail.com>
2024-12-16 17:49:44 +0300

    Update Pingpong.sh

--- Pingpong.sh
+++ Pingpong.sh
@@ -9,23 +9,15 @@ install_pingpong() {
     echo "PingPong dosyası indiriliyor..."
     wget -O PINGPONG https://pingpong-build.s3.ap-southeast-1.amazonaws.com/linux/latest/PINGPONG
 
-    # Screen oturumu oluştur
+    # Screen oturumu oluştur ve kaydedip çık
     echo "Screen oturumu açılıyor..."
     screen -dmS pingpong
-
-    # Screen içine komutu yaz ama çalıştırma
-    screen -S pingpong -X stuff "chmod +x ./PINGPONG && ./PINGPONG --key"
 
     # Screen'i detach et
     screen -S pingpong -X detach
 
     # Screen'e tekrar bağlan
     screen -r pingpong
-
-    # Script dosyasını sil
-    rm /root/Pingpong.sh
-
-    echo "Kurulum tamamlandı."
 }
 
 # Scripti çalıştır
