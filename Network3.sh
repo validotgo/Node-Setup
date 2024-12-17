02533f55a0fcea3f1785c55fe64985a90d688f3e
𝐯𝐚𝐥𝐢𝐝𝐨𝐭𝐠𝐨 <validotgo@mail.com>
2024-12-17 23:27:15 +0300

    Update Network3.sh

--- Network3.sh
+++ Network3.sh
@@ -10,10 +10,10 @@ cd ~
 sudo apt install -y wireguard
 
 # Download the Network3 Node Archive
-wget https://network3.io/ubuntu-node-v2.1.1.tar
+https://network3.io/ubuntu-node-v2.1.1.tar.gz
 
 # Extract the Archive
-tar -xf ubuntu-node-v2.1.1.tar
+tar -xf ubuntu-node-v2.1.1.tar.gz
 
 # Install Net Tools
 sudo apt-get install -y net-tools
@@ -29,7 +29,7 @@ sudo bash manager.sh key
 
 # Remove the ubuntu-node-v2.1.1.tar archive
 echo "Kurulum tamamlandı, ubuntu-node-v2.1.1.tar dosyası siliniyor..."
-rm -f ~/ubuntu-node-v2.1.1.tar
+rm -f ~/ubuntu-node-v2.1.1.tar.gz
 
 # Remove the script itself (Network3.sh)
 echo "Script dosyası (Network3.sh) siliniyor..."
