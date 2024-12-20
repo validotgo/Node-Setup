3be0d644ab26548ade427f7647c809d0143e8f5f
𝐯𝐚𝐥𝐢𝐝𝐨𝐭𝐠𝐨 <validotgo@mail.com>
2024-12-20 05:09:13 +0300

    Update New.sh

--- New.sh
+++ New.sh
@@ -39,12 +39,12 @@ services:
       - PASSWORD=123987Root
       - PUID=1000
       - PGID=1000
-      - TZ=Europe/London
+      - TZ=Etc/UTC
       - CHROME_CLI=
     volumes:
       - /root/chromium/config:/config
     ports:
-      - 3010:3000 
+      - 3010:3000
       - 3011:3001
     shm_size: "1gb"
     restart: unless-stopped
